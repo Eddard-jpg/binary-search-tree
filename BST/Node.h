eadef373612f6c0526d93803115c5e534b163e0e
Mostafa Emad <59015103+Eddard-jpg@users.noreply.github.com>
2023-02-17 18:19:41 +0200

    Update Node.h

--- BST/Node.h
+++ BST/Node.h
@@ -13,27 +13,36 @@ class Node {
 public:
     int value;
     Node *parent = nullptr;
-    unique_ptr<Node> leftChild = nullptr, rightChild = nullptr;
+    unique_ptr<Node> child[2];
     unique_ptr<Node> *myPtr = nullptr;
     
     bool operator<(const Node &other) const { return value < other.value; }
     bool operator==(const Node &other) const { return value == other.value; }
     bool operator>(const Node &other) const { return value > other.value; }
     
-    explicit Node(int value_) : value(value_) {
-        //cout << value << " Created" << endl;
+    explicit Node(int value_) : value(value_) {}
+    
+    Node(int value_, Node *parent_, unique_ptr<Node> &myPtr_) : value(value_), parent(parent_), myPtr(&myPtr_) {}
+    
+    // Add child node, direction = 0 for left, 1 for right.
+    void add_child(int value_, int direction) {
+        child[direction] = make_unique<Node>(value_, this, child[direction]);
     }
     
-    void addLeft(int value_) {
-        leftChild = make_unique<Node>(value_);
-        leftChild->parent = this;
-        leftChild->myPtr = &leftChild;
+    // Attach an existing node as a child, direction = 0 for left, 1 for right.
+    void attach_child(Node *node, int direction) {
+        child[direction].reset(node);
+        child[direction]->parent = this;
+        child[direction]->myPtr = &child[direction];
     }
     
-    void addRight(int value_) {
-        rightChild = make_unique<Node>(value_);
-        rightChild->parent = this;
-        rightChild->myPtr = &rightChild;
+    void attach_to(Node *parent_, unique_ptr<Node> &myPtr_) {
+        if (myPtr->get() == this)
+            myPtr_ = std::move(*myPtr);
+        else
+            myPtr_.reset(this);
+        parent = parent_;
+        myPtr = &myPtr_;
     }
     
     ~Node() {
